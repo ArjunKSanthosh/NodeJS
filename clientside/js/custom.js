d4fb845471492e2eee8491766e64756987cfab29
ArjunKSanthosh <arjunk80043@gmail.com>
2024-09-04 09:34:54 +0530

    Done

--- clientside/js/custom.js
+++ clientside/js/custom.js
@@ -1,1 +1,23 @@
-alert("hai")
+async function getDonors() {
+    const res=fetch("http://localhost:3000/getdonors")
+    const data=await (await res).json();
+    str=``
+    data.map((dt)=>{
+        str+=`<div class="contents">
+            <input type="text" name="name" id="name" placeholder="name" value=${dt.name}  class="ip">
+            <input type="text" name="email" id="email" placeholder="email" value=${dt.email} class="ip">
+            <input type="text" name="phone" id="phone" placeholder="phone" value=${dt.phone} class="ip">
+            <input type="text" name="bgroup" id="bgroup" placeholder="Blood Group" value=${dt.bgroup} class="ip">
+            <input type="text" name="gender" id="gender" placeholder="Gender" value=${dt.gender} class="ip">
+        </div>
+        <div class="bts">
+            <button style="background-color: blue;" class="bt2">Edit</button>
+            <button style="background-color: green;"class="bt2">Save</button>
+            <button style="background-color: red;" class="bt2"  onclick=`handleDelete()${_id}`>Delete</button>
+        </div>`
+    })
+    document.getElementById("inputs").innerHTML=str
+    
+}
+getDonors()
+async function handleDelete()
